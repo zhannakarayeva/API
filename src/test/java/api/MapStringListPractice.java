403a90dab47488746f38f9d3e0a836a143ee9295
zhannakarayeva <123839145+zhannakarayeva@users.noreply.github.com>
2023-04-29 16:44:35 -0500

    API Project

---
+++ src/test/java/api/MapStringListPractice.java
@@ -0,0 +1,13 @@
+package api;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class MapStringListPractice {
+    @Test
+    public void map() {
+
+    }
+}
---
+++ src/test/java/api/starWars/StarWarsCharacterFemailValidation.java
@@ -0,0 +1,15 @@
+package api.starWars;
+
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class StarWarsCharacterFemailValidation {
+
+}
